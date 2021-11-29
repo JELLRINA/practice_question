b5542e60570cb65ffb574a107c7c715f0eaee5e4
jellrina <dlgh6007@gmail.com>
2021-11-30 00:00:12 +0900

    소수

--- BaekJoon/src/BasicMath1/FlyMeToTheAlphaCentauri.java
+++ BaekJoon/src/BasicMath1/FlyMeToTheAlphaCentauri.java
@@ -10,21 +10,21 @@ public class FlyMeToTheAlphaCentauri {
 	public static void main(String[] args) throws IOException {
 		// 거리에 따른 장치 사용 횟수를 출력하는 문제
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder(); //한번에 출력
+		StringBuilder sb = new StringBuilder(); // 한번에 출력
 
 		int T = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < T; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " "); //문자열 분리
+			StringTokenizer st = new StringTokenizer(br.readLine(), " "); // 문자열 분리
 
-			//x는 항상 y보다 작은 값을 갖는다. (0 ≤ x < y < 231)
+			// x는 항상 y보다 작은 값을 갖는다. (0 ≤ x < y < 231)
 			int X = Integer.parseInt(st.nextToken());
 			int Y = Integer.parseInt(st.nextToken());
 
-			int distance = Y - X; // 거리 
+			int distance = Y - X; // 거리
 			int max = (int) Math.sqrt(distance); // 거리의 루트 값 int형으로.
-			
-			//최소한의 공간이동 장치 작동 횟수를 출력한다.
+
+			// 최소한의 공간이동 장치 작동 횟수를 출력한다.
 			if (max == Math.sqrt(distance)) {
 				sb.append(max * 2 - 1).append('\n'); // k-1,
 			} else if (distance <= max * max + max) {
---
+++ BaekJoon/src/BasicMath2/소수찾기.java
@@ -0,0 +1,43 @@
+package BasicMath2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 소수찾기 {
+
+	public static void main(String[] args) throws IOException {
+		// 2부터 X-1까지 모두 나눠서 X가 소수인지 판별하는 문제 1
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		br.readLine();
+		int cnt = 0;
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		while (st.hasMoreTokens()) {
+			boolean value = true;
+
+			int num = Integer.parseInt(st.nextToken());
+
+			if (num == 1) { // 1은 소수가 아니다.
+				continue;
+			}
+			//소수의 판별은 num-1이 아닌 2부터 나눈 num의 제곱근까지 검사
+			for (int i = 2; i <= Math.sqrt(num); i++) { 
+				if (num % i == 0) {
+					value = false;
+					break;
+				}
+			}
+			if (value) { // 아니면 소수라서 개수 +1증가
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+
+	}
+
+}
